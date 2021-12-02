295470b108260cdb959677704749b6e00db52ad1
dinesh0831 <81325951+dinesh0831@users.noreply.github.com>
2021-12-02 18:06:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static("cover"))
+app.use("/public",express.static(__dirname+../cover))
 
 
 app.use("/product",products)
