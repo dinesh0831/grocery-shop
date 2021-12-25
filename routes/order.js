8828d2a0734f65bf1b464c61dfa826bf03e54d8d
Dinesh <81325951+dinesh0831@users.noreply.github.com>
2021-12-25 18:46:02 +0530

    Add files via upload

--- routes/order.js
+++ routes/order.js
@@ -2,5 +2,6 @@ var express = require('express');
 var order=require("../module/order.service")
 var router = express.Router();
 router.post("/",order.postOrder);
-router.get("/getOrder",order.getById)
+router.get("/getOrder",order.getById);
+router.post("/razorPay",order.razorPay)
 module.exports=router
